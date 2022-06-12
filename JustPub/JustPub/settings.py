e20209227232773f72d5fcd6111b7be8ff8dbb4d
Eliot <halychprogramming@gmail.com>
2022-06-12 16:38:52 +0300

    First attempt dockerizing

--- JustPub/JustPub/settings.py
+++ JustPub/JustPub/settings.py
@@ -1,3 +1,4 @@
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -6,12 +7,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8(5cute6uh)wql1mi*k(@3s6)1iou-0siye^i3oe20gud91)do'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
