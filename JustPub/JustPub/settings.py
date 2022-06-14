626c7133b0f94722ac93f9c2693ddab8b5ad7898
Eliot <halychprogramming@gmail.com>
2022-06-15 02:07:39 +0300

    Removed shi*ty file and added redirect middleware instead.

--- JustPub/JustPub/settings.py
+++ JustPub/JustPub/settings.py
@@ -39,6 +39,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'Pub.redirectMiddleware.RedirectMiddleware'
 ]
 
 ROOT_URLCONF = 'JustPub.urls'
